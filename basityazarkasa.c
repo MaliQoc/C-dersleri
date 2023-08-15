bef50385c4d59e745fbb2e2e45ed2c9803501a56
Ali Hasan Ozan Koç <84283749+MaliQoc@users.noreply.github.com>
2023-08-15 23:22:38 +0300

    5. ders

--- basityazarkasa.c
+++ basityazarkasa.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	
-	printf("*****YAZARKASA*****\n\n");
+  printf("*****YAZARKASA*****\n\n");
 	
   int a,b,c,d;
   
@@ -15,26 +13,26 @@ int main(int argc, char *argv[]) {
   scanf("%d",&a);
   b=a*40;
   
-  if(a>0 && a<=10)
+  if (a>0 && a<=10)
   {
   	c=b*10/100;
   	d=b-c;
   	printf("fiyat: %d TL",d);
   }
-  if(a>10 && a<=20)
+  if (a>10 && a<=20)
   {
   	c=b*20/100;
   	d=b-c;
     printf("fiyat: %d TL",d);
   }
-  if(a>20)
+  if (a>20)
   {
   	c=b*30/100;
   	d=b-c;
   	printf("fiyat: %d TL",d);
   }
     
-    // Satılan kitap sayısına göre yapılacak indirimler vardır. Buna göre net fiyat hesabını yapacak kodu yazmamız istenmiş.
+// SatÄ±lan kitap sayÄ±sÄ±na gÃ¶re yapÄ±lacak indirimler vardÄ±r. Buna gÃ¶re net fiyat hesabÄ±nÄ± yapacak kodu yazmamÄ±z istenmiÅŸ.
 	 
-	return 0;
+return 0;
 }
